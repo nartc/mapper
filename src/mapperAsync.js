b5d951c64a9d169392a0451ebfbaba3409cfeb43
Chau Tran <ctch5@mail.umsl.edu>
2019-12-04 23:02:42 -0600

    fix(mapperasync.js): test async

--- src/mapperAsync.js
+++ src/mapperAsync.js
@@ -1,4 +1,27 @@
 const path = require('path');
+const { parentPort, workerData } = require('worker_threads');
+const { _map, _mapArray } = require(path.resolve(__dirname, './base'));
+const { _toArrayBuffer, _toString } = require(path.resolve(
+  __dirname,
+  './utils/common.utils'
+));
 
-require('ts-node').register();
-require(path.resolve(__dirname, './mapperAsync.ts'));
+const { sourceObj, mapping, options, mappings, isMapArray } = JSON.parse(
+  _toString(workerData)
+);
+
+if (parentPort) {
+  if (isMapArray) {
+    parentPort.postMessage(
+      _toArrayBuffer(
+        JSON.stringify(_mapArray(sourceObj, mapping, options, mappings))
+      )
+    );
+  } else {
+    parentPort.postMessage(
+      _toArrayBuffer(
+        JSON.stringify(_map(sourceObj, mapping, options, false, mappings))
+      )
+    );
+  }
+}

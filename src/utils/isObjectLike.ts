7970ea496455a9c66c7c6c362337a147df624306
Chau Tran <ctch5@mail.umsl.edu>
2020-03-20 16:38:10 -0500

    fix(isobjectlike.ts): fix isObjectlike

--- src/utils/isObjectLike.ts
+++ src/utils/isObjectLike.ts
@@ -2,5 +2,9 @@ import { ObjectTag } from '../types';
 import { getTag } from './getTag';
 
 export function isObjectLike(obj: any): boolean {
-  return getTag(obj) === ObjectTag.Object || getTag(obj) === ObjectTag.Array;
+  return (
+    getTag(obj) === ObjectTag.Object ||
+    getTag(obj) === ObjectTag.Array ||
+    getTag(obj) === ObjectTag.Date
+  );
 }

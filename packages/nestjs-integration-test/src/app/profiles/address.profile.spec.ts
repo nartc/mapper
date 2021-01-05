7d9aa00d0b9782b476b443ab9760f989a2bfadc5
Chau Tran <nartc7789@gmail.com>
2021-01-04 18:01:56 -0600

    test(nestjs-integration-test): add sample profile test

---
+++ packages/nestjs-integration-test/src/app/profiles/address.profile.spec.ts
@@ -0,0 +1,35 @@
+import { classes } from '@automapper/classes';
+import { createMapper } from '@automapper/core';
+import { getMapperToken } from '@automapper/nestjs';
+import type { Mapper } from '@automapper/types';
+import { Test } from '@nestjs/testing';
+import { Address, AddressVm } from '../models/address';
+import { AddressProfile } from './address.profile';
+
+describe('addressProfile', () => {
+  let mapper: Mapper;
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getMapperToken(),
+          useValue: createMapper({ name: '', pluginInitializer: classes }),
+        },
+        AddressProfile,
+      ],
+    }).compile();
+    mapper = moduleRef.get<Mapper>(getMapperToken());
+  });
+
+  it('', () => {
+    const address = new Address();
+    address.street = 'street';
+    address.city = 'city';
+    address.state = 'state';
+
+    const vm = mapper.map(address, AddressVm, Address);
+    expect(vm.formattedAddress).toEqual(
+      `${address.street} ${address.city} ${address.state}`
+    );
+  });
+});

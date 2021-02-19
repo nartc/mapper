5bcc75d8060d01453c99392c5e38e2eef495f9d5
Chau Tran <nartc7789@gmail.com>
2021-02-18 18:10:18 -0600

    test(experimental/transformer-plugin): add test for transformer plugin

---
+++ packages/integration-test/src/lib/trasnformer-plugin/fixtures/user.model.ts
@@ -0,0 +1,116 @@
+export const userModelText = `
+enum Role {
+  ADMIN,
+  USER
+}
+enum StringEnum {
+  FOO = "foo",
+  BAR = "bar"
+}
+class Address {
+  street: string;
+}
+class Profile {
+  bio: string;
+  age: string;
+  role: Role;
+  roles: Role[];
+  maybeRole?: Role;
+  maybeRoles?: Role[];
+  nullableRole: Role | null;
+  stringEnum: StringEnum;
+  stringEnums: StringEnum[];
+  maybeStringEnum?: StringEnum;
+  maybeStringEnums?: StringEnum[];
+  nullableStringEnum: StringEnum | null;
+}
+export class User {
+  firstName: string;
+  lastName: string;
+  profile: Profile;
+  addresses: Address[];
+  otherAddresses: Array<Address>;
+  flag: boolean;
+  foo: any;
+  nullable: string | null;
+  primitives: string[];
+  nullableType: Address | null;
+  maybePrimitives?: string[];
+  nullablePrimitives: string[] | null;
+  maybeType?: Address;
+}
+`;
+
+export const userModelTextStrict = `
+enum Role {
+  ADMIN,
+  USER
+}
+enum StringEnum {
+  FOO = "foo",
+  BAR = "bar"
+}
+class Address {
+  street!: string;
+}
+class Profile {
+  bio!: string;
+  age!: string;
+  role!: Role;
+  roles!: Role[];
+  maybeRole?: Role;
+  maybeRoles?: Role[];
+  nullableRole!: Role | null;
+  stringEnum!: StringEnum;
+  stringEnums!: StringEnum[];
+  maybeStringEnum?: StringEnum;
+  maybeStringEnums?: StringEnum[];
+  nullableStringEnum!: StringEnum | null;
+}
+export class User {
+  firstName!: string;
+  lastName!: string;
+  profile!: Profile;
+  addresses!: Address[];
+  otherAddresses!: Array<Address>;
+  flag!: boolean;
+  foo!: any;
+  nullable!: string | null;
+  primitives!: string[];
+  nullableType!: Address | null;
+  maybePrimitives?: string[];
+  nullablePrimitives!: string[] | null;
+  maybeType?: Address;
+}
+`;
+
+export const userModelTranspiledText = `"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["USER"] = 1] = "USER";
+})(Role || (Role = {}));
+var StringEnum;
+(function (StringEnum) {
+    StringEnum["FOO"] = "foo";
+    StringEnum["BAR"] = "bar";
+})(StringEnum || (StringEnum = {}));
+class Address {
+    static __AUTOMAPPER_METADATA_FACTORY__() {
+        return [["street", { typeFn: () => String }]];
+    }
+}
+class Profile {
+    static __AUTOMAPPER_METADATA_FACTORY__() {
+        return [["bio", { typeFn: () => String }], ["age", { typeFn: () => String }], ["role", { typeFn: () => Number }], ["roles", { typeFn: () => Number }], ["maybeRole", { typeFn: () => Number }], ["maybeRoles", { typeFn: () => Number }], ["nullableRole", { typeFn: () => Number }], ["stringEnum", { typeFn: () => String }], ["stringEnums", { typeFn: () => String }], ["maybeStringEnum", { typeFn: () => String }], ["maybeStringEnums", { typeFn: () => String }], ["nullableStringEnum", { typeFn: () => String }]];
+    }
+}
+class User {
+    static __AUTOMAPPER_METADATA_FACTORY__() {
+        return [["firstName", { typeFn: () => String }], ["lastName", { typeFn: () => String }], ["profile", { typeFn: () => Profile, depth: 0 }], ["addresses", { typeFn: () => Address, depth: 0 }], ["otherAddresses", { typeFn: () => Address, depth: 0 }], ["flag", { typeFn: () => Boolean }], ["foo", { typeFn: () => null, depth: 0 }], ["nullable", { typeFn: () => String }], ["primitives", { typeFn: () => String }], ["nullableType", { typeFn: () => Address, depth: 0 }], ["maybePrimitives", { typeFn: () => String }], ["nullablePrimitives", { typeFn: () => String }], ["maybeType", { typeFn: () => Address, depth: 0 }]];
+    }
+}
+exports.User = User;
+`;
---
+++ packages/integration-test/src/lib/trasnformer-plugin/transformer-plugin.spec.ts
@@ -0,0 +1,54 @@
+import automapperTransformerPlugin from '@automapper/experimental/transformer-plugin';
+import * as tss from 'typescript/lib/tsserverlibrary';
+import {
+  userModelText,
+  userModelTextStrict,
+  userModelTranspiledText,
+} from './fixtures/user.model';
+
+describe('Transformer Plugin', () => {
+  it('compile', () => {
+    const tsConfig: tss.CompilerOptions = {
+      module: tss.ModuleKind.CommonJS,
+      target: tss.ScriptTarget.ESNext,
+      noEmitHelpers: true,
+    };
+
+    const fileName = 'user.model.ts';
+    const programFixture = tss.createProgram([fileName], tsConfig);
+
+    const result = tss.transpileModule(userModelText, {
+      compilerOptions: tsConfig,
+      fileName,
+      transformers: {
+        before: [automapperTransformerPlugin(programFixture).before],
+      },
+    });
+
+    expect(result.outputText).toBeTruthy();
+    expect(result.outputText).toEqual(userModelTranspiledText);
+  });
+
+  it('compile strict', () => {
+    const tsConfig: tss.CompilerOptions = {
+      module: tss.ModuleKind.CommonJS,
+      target: tss.ScriptTarget.ESNext,
+      noEmitHelpers: true,
+      strict: true,
+    };
+
+    const fileName = 'user.model.ts';
+    const programFixture = tss.createProgram([fileName], tsConfig);
+
+    const result = tss.transpileModule(userModelTextStrict, {
+      compilerOptions: tsConfig,
+      fileName,
+      transformers: {
+        before: [automapperTransformerPlugin(programFixture).before],
+      },
+    });
+
+    expect(result.outputText).toBeTruthy();
+    expect(result.outputText).toEqual(userModelTranspiledText);
+  });
+});

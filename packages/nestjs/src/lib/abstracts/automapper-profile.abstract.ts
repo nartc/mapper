70b268cd6f93aab4592253eb48a54df10554214c
Chau Tran <nartc7789@gmail.com>
2021-02-22 09:38:44 -0600

    fix(nestjs): move addProfile code to the micro task queue
    delay the addProfile operation for the DI to resolve all dependencies of Profile classes
    
    #264

--- packages/nestjs/src/lib/abstracts/automapper-profile.abstract.ts
+++ packages/nestjs/src/lib/abstracts/automapper-profile.abstract.ts
@@ -5,7 +5,7 @@ export abstract class AutomapperProfile {
 
   protected constructor(mapper: Mapper) {
     this.mapper = mapper;
-    this.mapper.addProfile(this.mapProfile());
+    Promise.resolve().then(() => this.mapper.addProfile(this.mapProfile()));
   }
 
   abstract mapProfile(): MappingProfile;

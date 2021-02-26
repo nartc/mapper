ebd53dcf2ddf4562b0645d9f55720f980398e710
Chau Tran <nartc7789@gmail.com>
2021-02-26 16:47:41 -0600

    fix(classes): make sure to reassign metadataList

--- packages/classes/src/lib/classes.ts
+++ packages/classes/src/lib/classes.ts
@@ -165,11 +165,11 @@ function exploreMetadata(
     // if metadataStorage hasn't had metadata of the model
     if (!metadataStorage.has(model)) {
       // get the metadata from Reflection and AUTOMAPPER_METADATA_FACTORY then populate metadataStorage and instanceStorage
-      const metadataList =
+      let metadataList =
         Reflect.getMetadata(AUTOMAP_PROPERTIES_METADATA_KEY, model) || [];
 
       if ((model as any)[AUTOMAPPER_METADATA_FACTORY_KEY]) {
-        metadataList.concat(
+        metadataList = metadataList.concat(
           (model as any)[AUTOMAPPER_METADATA_FACTORY_KEY]() || []
         );
       }

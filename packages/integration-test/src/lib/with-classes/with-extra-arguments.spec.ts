54fe31ba4bd7e7e293b5b3f66e552800960e5b73
Chau Tran <nartc7789@gmail.com>
2021-03-22 08:05:08 -0500

    test(integration-test): add test cases for nested mapping needing extraArguments

--- packages/integration-test/src/lib/with-classes/with-extra-arguments.spec.ts
+++ packages/integration-test/src/lib/with-classes/with-extra-arguments.spec.ts
@@ -11,7 +11,13 @@ describe('withExtraArguments', () => {
   const [mapper] = setupClasses('withExtraArguments');
 
   beforeEach(() => {
-    mapper.createMap(SimpleBar, SimpleBarVm);
+    mapper.createMap(SimpleBar, SimpleBarVm).forMember(
+      (d) => d.bar,
+      mapWithArguments(
+        (source, { concat = 'default concat' }: { concat?: string }) =>
+          source.bar + ' ' + concat
+      )
+    );
     mapper.createMap(SimpleFoo, SimpleFooVm).forMember(
       (d) => d.fooBar,
       mapWithArguments(
@@ -69,6 +75,61 @@ describe('withExtraArguments', () => {
     vms.forEach((vm, index) => {
       expect(vm.foo).toEqual(foos[index].foo);
       expect(vm.fooBar).toEqual(foos[index].fooBar * 3);
+    });
+  });
+
+  it('should map with args for nested Bar', () => {
+    const foo = new SimpleFoo();
+    foo.foo = 'foo';
+    foo.fooBar = 2;
+    foo.bar = new SimpleBar();
+    foo.bar.bar = 'bar';
+
+    let vm = mapper.map(foo, SimpleFooVm, SimpleFoo, {
+      extraArguments: { multiplier: 3 },
+    });
+    expect(vm.foo).toEqual(foo.foo);
+    expect(vm.fooBar).toEqual(foo.fooBar * 3);
+    expect(vm.bar.bar).toEqual(foo.bar.bar + ' default concat');
+
+    vm = mapper.map(foo, SimpleFooVm, SimpleFoo, {
+      extraArguments: { multiplier: 2, concat: 'concat this instead' },
+    });
+    expect(vm.fooBar).toEqual(foo.fooBar * 2);
+    expect(vm.bar.bar).toEqual(foo.bar.bar + ' concat this instead');
+  });
+
+  it('should mapArray with args for nested Bar', () => {
+    const foo = new SimpleFoo();
+    foo.foo = 'foo';
+    foo.fooBar = 2;
+    foo.bar = new SimpleBar();
+    foo.bar.bar = 'bar';
+
+    const foo2 = new SimpleFoo();
+    foo2.foo = 'foo2';
+    foo2.fooBar = 3;
+    foo2.bar = new SimpleBar();
+    foo2.bar.bar = 'bar2';
+
+    const foos = [foo, foo2];
+
+    let vms = mapper.mapArray(foos, SimpleFooVm, SimpleFoo, {
+      extraArguments: { multiplier: 3 },
+    });
+
+    vms.forEach((vm, index) => {
+      expect(vm.fooBar).toEqual(foos[index].fooBar * 3);
+      expect(vm.bar.bar).toEqual(foos[index].bar.bar + ' default concat');
+    });
+
+    vms = mapper.mapArray(foos, SimpleFooVm, SimpleFoo, {
+      extraArguments: { multiplier: 2, concat: 'concat this instead' },
+    });
+
+    vms.forEach((vm, index) => {
+      expect(vm.fooBar).toEqual(foos[index].fooBar * 2);
+      expect(vm.bar.bar).toEqual(foos[index].bar.bar + ' concat this instead');
     });
   });
 });

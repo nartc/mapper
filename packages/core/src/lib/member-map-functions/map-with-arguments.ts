8f1dfc6f27d829aec16e0ffc79d31edab6316569
Chau Tran <nartc7789@gmail.com>
2021-04-07 12:12:44 -0500

    feat(core): allow mapWithArguments to accept Resolver

--- packages/core/src/lib/member-map-functions/map-with-arguments.ts
+++ packages/core/src/lib/member-map-functions/map-with-arguments.ts
@@ -1,19 +1,29 @@
 import type {
   Dictionary,
   MapWithArgumentsReturn,
+  Resolver,
   SelectorReturn,
 } from '@automapper/types';
 import { TransformationType } from '@automapper/types';
+import { isResolver } from '../utils';
 
 export function mapWithArguments<
   TSource extends Dictionary<TSource> = any,
   TDestination extends Dictionary<TDestination> = any,
   TSelectorReturn = SelectorReturn<TDestination>
 >(
-  withArgumentsResolver: (
-    source: TSource,
-    extraArguments: Record<string, unknown>
-  ) => TSelectorReturn
+  withArgumentsResolver:
+    | ((
+        source: TSource,
+        extraArguments: Record<string, unknown>
+      ) => TSelectorReturn)
+    | Resolver<TSource, Record<string, unknown>, TSelectorReturn>
 ): MapWithArgumentsReturn<TSource, TDestination, TSelectorReturn> {
+  if (isResolver(withArgumentsResolver)) {
+    return [
+      TransformationType.MapWithArguments,
+      withArgumentsResolver.resolve.bind(withArgumentsResolver),
+    ];
+  }
   return [TransformationType.MapWithArguments, withArgumentsResolver];
 }

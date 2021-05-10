5e10eeac706fa54dfc1435ecc1abdf4c5c31a7dc
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-05-10 19:54:53 -0400

    feat(core): improve `getMembersFromArrowFunctionExpr` to avoid map and match all the away

--- packages/core/src/lib/create-mapper/get-members.util.ts
+++ packages/core/src/lib/create-mapper/get-members.util.ts
@@ -10,52 +10,51 @@
  * ["foo", "'bar'"]
  * ```
  *
- * To understand this regex, read the comments at the end of this file.
+ * ### Explanation:
+ * ```
+ * (?:        // (begin of non-capturing group)
+ *   (?<=     // (begin of positive lookbehind)
+ *     \[     // matches a  literal "[" but without including it in the match result
+ *   )        // (end of positive lookbehind)
+ *   (        // (begin capturing group #1)
+ *     ['"]   // match one occurrence of a single or double quotes characters
+ *   )        // (end capturing group #1)
+ *   (        // (begin capturing group #2)
+ *     .*?    // followed by 0 or more of any character, but match as few characters as possible (which is 0)
+ *   )        // (end capturing group #2)
+ *   \1       // followed by the result of capture group #1
+ * )          // (end of non-capturing group)
+ * |          // Or matches with...
+ * (?:        // (begin of non-capturing group)
+ *   (?<=     // (begin of positive lookbehind)
+ *     \.     // matches a literal "." but without including it in the match result
+ *   )        // (end of positive lookbehind)
+ *   [^.[]+   // followed by 1 or more occurrences of any character but "." nor "["
+ * )          // (end of non-capturing group)
+ * ```
  */
-const RE_ARROW_FN_SELECTOR_PROPS = /(?:(?<=\[)(['"]).*?\1)|(?:(?<=\.)[^.[]+)/g;
+const RE_ARROW_FN_SELECTOR_PROPS = /(?:(?<=\[)(['"])(.*?)\1)|(?:(?<=\.)[^.[]+)/g;
 
 /**
  * @param {string} fnSelectorStr A serialized and cleaned arrow function expression.
- * @returns Will return `null` if the given `fnSelector` doesn't match with anything.
+ * @returns `null` if the given `fnSelector` doesn't match with anything.
  */
 export function getMembersFromArrowFunctionExpr(
   fnSelectorStr: string
 ): string[] | null {
-  const matches = fnSelectorStr.match(RE_ARROW_FN_SELECTOR_PROPS);
-  // May be we could optimise the following by using `.exec`
-  // to prevent doing `.map` and `.match`
-  if (matches) {
-    const RE_HAS_QUOTES = /^(['"]).*\1$/;
-    return matches.map((match) =>
-      match.match(RE_HAS_QUOTES)
-        ? match.slice(1, -1) // drop the first and the last quotes
-        : match
-    );
-  }
+  let matches = RE_ARROW_FN_SELECTOR_PROPS.exec(fnSelectorStr);
 
-  return null;
+  if (!matches) return null;
+
+  const members: string[] = [];
+  do {
+    // Use the value of the second captured group or the entire match, since
+    // we do not want to capture the matching quotes (when any)
+    const propFound = matches[2] ?? matches[0];
+    //                           ^^ Using the nullish operator since the left
+    //                              side could be an empty string, which is falsy.
+    members.push(propFound);
+  } while ((matches = RE_ARROW_FN_SELECTOR_PROPS.exec(fnSelectorStr)));
+
+  return members;
 }
-
-/******************************************************************************
-
-// Explanation of RE_ARROW_FN_SELECTOR_PROPS regex
-
-(?:        # (begin of non-capturing group)
-  (?<=     # (begin of positive lookbehind)
-    \[     # matches a  literal "[" but without including it in the match result
-  )        # (end of positive lookbehind)
-  (        # (begin capturing group #1)
-    ['"]   # match one occurrence of a single or double quotes characters
-  )        # (end capturing group #1)
-  .*?      # followed by 0 or more of any character, but match as few characters as possible (which is 0)
-  \1       # followed by the result of capture group #1
-)          # (end of non-capturing group)
-|          # Or matches with...
-(?:        # (begin of non-capturing group)
-  (?<=     # (begin of positive lookbehind)
-    \.     # matches a literal "." but without including it in the match result
-  )        # (end of positive lookbehind)
-  [^.[]+   # followed by 1 or more occurrences of any character but "." nor "["
-)          # (end of non-capturing group)
-
-******************************************************************************/

93d3f66a97313fdbdb1798d789e366fba4e501ac
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2021-05-17 23:21:28 -0400

    feat(core): add partial support for ES6 computed property names (#293)
    * test(core): add empty string and trailing dot paths cases
    
    * feat(core): add a partial support for computed property names
    
    Computed property names were introduced in ES6.
    
    "Partial" in a sense that few computed property name usage are not
    supported. As well for when using with ES5 functions, not arrow
    functions.
    
    Closes #289
    
    * docs: adjust odd-property name limitations
    
    * feat(core): improve `getMembersFromArrowFunctionExpr` to avoid map and match all the away
    
    * feat(core): add suport to computed property names for ES5
    
    * docs: remove the ES5 limitation of odd-property name
    
    * feat(core): reset the state of the shared regex before
    
    Since we are using a constant regex that has the `g` flag in iterator
    we should make sure that it is in the initial state.
    See http://speakingjs.com/es5/ch19.html

--- packages/core/src/lib/create-mapper/get-member-path.util.ts
+++ packages/core/src/lib/create-mapper/get-member-path.util.ts
@@ -1,42 +1,103 @@
 import type { Selector } from '@automapper/types';
 
-// TODO: support odd properties like: 'odd-property', 'odd.property'?
+/**
+ * An regular expression to match will all property names of a given cleaned
+ * arrow function expression. Note that if there is some computed names thus
+ * they are returning if the quotes.
+ *
+ * @example
+ * ```js
+ * "s=>s.foo['bar']".match(RE_ARROW_FN_SELECTOR_PROPS)
+ * // will return
+ * ["foo", "'bar'"]
+ * ```
+ *
+ * ### Explanation:
+ * ```
+ * (?:        // (begin of non-capturing group)
+ *   (?<=     // (begin of positive lookbehind)
+ *     \[     // matches a  literal "[" but without including it in the match result
+ *   )        // (end of positive lookbehind)
+ *   (        // (begin capturing group #1)
+ *     ['"]   // match one occurrence of a single or double quotes characters
+ *   )        // (end capturing group #1)
+ *   (        // (begin capturing group #2)
+ *     .*?    // followed by 0 or more of any character, but match as few characters as possible (which is 0)
+ *   )        // (end capturing group #2)
+ *   \1       // followed by the result of capture group #1
+ * )          // (end of non-capturing group)
+ * |          // Or matches with...
+ * (?:        // (begin of non-capturing group)
+ *   (?<=     // (begin of positive lookbehind)
+ *     \.     // matches a literal "." but without including it in the match result
+ *   )        // (end of positive lookbehind)
+ *   [^.[]+   // followed by 1 or more occurrences of any character but "." nor "["
+ * )          // (end of non-capturing group)
+ * ```
+ */
+const RE_FN_SELECTOR_PROPS = /(?:(?<=\[)(['"])(.*?)\1)|(?:(?<=\.)[^.[]+)/g;
+
+/**
+ * For a given cleaned and serialzed JS function selector expression, return a
+ * list of all members that were selected.
+ *
+ * @returns `null` if the given `fnSelector` doesn't match with anything.
+ */
+export function getMembers(fnSelectorStr: string): string[] | null {
+  // Making sure that the shared constant `/g` regex is in its initial state.
+  RE_FN_SELECTOR_PROPS.lastIndex = 0;
+
+  let matches = RE_FN_SELECTOR_PROPS.exec(fnSelectorStr);
+
+  if (!matches) return null;
+
+  const members: string[] = [];
+  do {
+    // Use the value of the second captured group or the entire match, since
+    // we do not want to capture the matching quotes (when any)
+    const propFound = matches[2] ?? matches[0];
+    //                           ^^ Using the nullish operator since the left
+    //                              side could be an empty string, which is falsy.
+    members.push(propFound);
+  } while ((matches = RE_FN_SELECTOR_PROPS.exec(fnSelectorStr)));
+
+  return members;
+}
+
+/**
+ * Get a dot-separated string of the properties selected by a given `fn` selector
+ * function.
+ *
+ * @example
+ * ```js
+ * getMemberPath(s => s.foo.bar) === 'foo.bar'
+ * getMemberPath(s => s['foo']) === 'foo'
+ * getMemberPath(s => s.foo['']) === 'foo.'
+ * // invalid usage
+ * getMemberPath(s => s) === ''
+ * ```
+ */
 export function getMemberPath(fn: Selector): string {
   const fnString = fn
     .toString()
     // .replace(/\/\* istanbul ignore next \*\//g, '')
     .replace(/cov_.+\n/g, '');
 
-  // ES6 prop selector:
-  // "x => x.prop"
-  if (fnString.includes('=>')) {
-    const cleaned = cleanseAssertionOperators(
-      fnString.substring(fnString.indexOf('.') + 1)
-    );
+  const cleaned = cleanseAssertionOperators(fnString);
 
-    if (cleaned.includes('=>')) return '';
-    return cleaned;
-  }
+  // Note that we don't need to remove the `return` keyword because, for instance,
+  // `(x) => { return x.prop }` will be turn into `x=>returnx.prop` (cleaned)
+  // thus we'll still be able to get only the string `prop` properly.
+  // The same for `function (x){}`
+  const members = getMembers(cleaned);
 
-  // ES5 prop selector:
-  // "function (x) { return x.prop; }"
-  // webpack production build excludes the spaces and optional trailing semicolon:
-  //   "function(x){return x.prop}"
-  // FYI - during local dev testing i observed carriage returns after the curly brackets as well
-  // Note by maintainer: See https://github.com/IRCraziestTaxi/ts-simple-nameof/pull/13#issuecomment-567171802 for
-  // explanation of this regex.
-  const matchRegex = /function\s*\(\w+\)\s*{[\r\n\s]*return\s+\w+\.((\w+\.)*(\w+))/i;
-
-  const es5Match = fnString.match(matchRegex);
-
-  if (es5Match) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return es5Match[1]!;
-  }
-
-  return '';
+  return members ? members.join('.') : '';
 }
 
+/**
+ * @returns {string} The given `parseName` but without curly brackets, blank
+ * spaces, semicolons, parentheses, "?" and "!" characters.
+ */
 function cleanseAssertionOperators(parsedName: string): string {
-  return parsedName.replace(/[?!]/g, '').replace(/(?:\s|;|{|}|\(|\)|)+/gm, '');
+  return parsedName.replace(/[\s{}()?!;]+/gm, '');
 }
--- packages/core/src/lib/create-mapper/specs/get-member-path.spec.ts
+++ packages/core/src/lib/create-mapper/specs/get-member-path.spec.ts
@@ -1,48 +1,190 @@
-import { getMemberPath } from '../get-member-path.util';
+import { getMembers, getMemberPath } from '../get-member-path.util';
+
+describe('getMembers', () => {
+  describe('cases that are allowed', () => {
+    const cases = [
+      [(something) => something.foo, ['foo']],
+      [(s) => s.foo, ['foo']],
+      [(s) => s.foo.bar, ['foo', 'bar']],
+      [(s) => s.returnFoo, ['returnFoo']],
+      [(s) => s.return_foo, ['return_foo']],
+
+      [(s) => s[' aaa '], [' aaa ']],
+      [(s) => s['aaa'], ['aaa']],
+      [(s) => s['ab-cd'], ['ab-cd']],
+      [(s) => s['ab_cd'], ['ab_cd']],
+
+      [(s) => s['bbb'].bar, ['bbb', 'bar']],
+      [(s) => s.bbb['bar'], ['bbb', 'bar']],
+
+      [(s) => s['aaa']['ddd'], ['aaa', 'ddd']],
+      [(s) => s['aaa'].ccc['ddd'], ['aaa', 'ccc', 'ddd']],
+
+      [(s) => s['.foo.bar'], ['.foo.bar']],
+      [(s) => s['foo=>s[bar'], ['foo=>s[bar']],
+      [(s) => s["['a']"], ["['a']"]],
+      [(s) => s['bad[foo]'], ['bad[foo]']],
+      [(s) => s.bar['bad[foo]'], ['bar', 'bad[foo]']],
+      [(s) => s["'a"], ["'a"]],
+      [(s) => s['aa']['bo"o'], ['aa', 'bo"o']],
+
+      [(s) => s['with_sṕéçiâl_chàrs'], ['with_sṕéçiâl_chàrs']],
+
+      [(s) => s['á'], ['á']],
+      [(s) => s.á, ['á']],
+      [(s) => s.good.á, ['good', 'á']],
+      [(s) => s['good'].á, ['good', 'á']],
+      [(s) => s.á['good'], ['á', 'good']],
+
+      [(s) => s[''], ['']],
+      [(s) => s.foo[''], ['foo', '']],
+      [(s) => s[''].foo, ['', 'foo']],
+    ].map<[serializedSelector: string, members: string[]]>(([fn, expected]) => [
+      fn.toString(),
+      expected as string[],
+    ]);
+
+    test.each(cases)(
+      'for "%s" should return %p list',
+      (fnSelector, expectedOutput) => {
+        const members = getMembers(fnSelector);
+        expect(members).toStrictEqual(expectedOutput);
+      }
+    );
+  });
+
+  describe('cases that are disallowed', () => {
+    const cases = [
+      [(s) => s, null], // Not a real one tho
+      [(s) => s`foo`, null],
+
+      // Known limitations that should be avoided in user land code because
+      // they will produce wrong outputs and cannot be detected beforehand
+      [(s) => s['fo' + 'o'], ['fo']], // expected to be ['foo']
+      // eslint-disable-next-line no-constant-condition
+      [(s) => s[true ? 'foo' : 'bar'], null], // expected to be ['foo']
+      [(s) => s[true && 'foo'], null], // expected to be ['foo']
+
+      [(s) => s[Symbol()], null], // I'm not sure if we should support this
+      [(s) => s[`a`], null], // To discourage the use of computed names
+    ].map<[serializedSelector: string, members: string[]]>(([fn, expected]) => [
+      fn.toString(),
+      expected as string[],
+    ]);
+
+    test.each(cases)(
+      'for "%s" should return %p',
+      (fnSelector, expectedOutput) => {
+        const members = getMembers(fnSelector);
+        expect(members).toStrictEqual(expectedOutput);
+      }
+    );
+  });
+});
 
 describe('getMemberPath', () => {
   interface Foo {
     foo: string;
+
     bar: {
       baz: string;
+      ['']: string;
     };
+
     returnFoo: string;
+    snake_case: string;
+
     'odd-property': string;
+    'even[odd].prop': string;
+    á: string;
+    with_sṕéçiâl_chàrs: string;
   }
 
   it('should return properly for ES6 arrow syntax', () => {
-    const path = getMemberPath((s: Foo) => s.foo);
+    let path: ReturnType<typeof getMemberPath>;
+
+    const namedOne = (s: Foo) => s.foo;
+    path = getMemberPath(namedOne);
     expect(path).toEqual('foo');
+
+    const namedOne2 = (s: Foo) => s['foo'];
+    path = getMemberPath(namedOne2);
+    expect(path).toEqual('foo');
+
+    path = getMemberPath((s: Foo) => s['snake_case']);
+    expect(path).toEqual('snake_case');
+
+    path = getMemberPath((s: Foo) => s.snake_case);
+    expect(path).toEqual('snake_case');
+
+    path = getMemberPath((s: Foo) => s['odd-property']);
+    expect(path).toEqual('odd-property');
+
+    path = getMemberPath((s: Foo) => s.snake_case);
+    expect(path).toEqual('snake_case');
+
+    path = getMemberPath((s: Foo) => s['even[odd].prop']);
+    expect(path).toEqual('even[odd].prop');
+
+    path = getMemberPath((s: Foo) => s.á);
+    expect(path).toEqual('á');
+
+    path = getMemberPath((s: Foo) => s['with_sṕéçiâl_chàrs']);
+    expect(path).toEqual('with_sṕéçiâl_chàrs');
   });
 
   it('should return properly for nested path for ES6 arrow syntax', () => {
-    const path = getMemberPath((s: Foo) => s.bar.baz);
+    let path: ReturnType<typeof getMemberPath>;
+
+    path = getMemberPath((s: Foo) => s.bar.baz);
     expect(path).toEqual('bar.baz');
+
+    path = getMemberPath((s: Foo) => s['bar']['baz']);
+    expect(path).toEqual('bar.baz');
+
+    path = getMemberPath((s: Foo) => s.bar.baz['']);
+    expect(path).toEqual('bar.baz.');
   });
 
   it('should return properly for ES5 function syntax', () => {
-    const path = getMemberPath(function (s: Foo) {
+    let path: ReturnType<typeof getMemberPath>;
+
+    path = getMemberPath(function (s: Foo) {
       return s.foo;
+    });
+    expect(path).toEqual('foo');
+
+    path = getMemberPath(function namedOne(s: Foo) {
+      return s.foo;
+    });
+    expect(path).toEqual('foo');
+
+    path = getMemberPath(function (s: Foo) {
+      return s['foo'];
     });
     expect(path).toEqual('foo');
   });
 
   it('should return properly for nested path for ES5 function syntax', () => {
-    const path = getMemberPath(function (s: Foo) {
+    let path: ReturnType<typeof getMemberPath>;
+
+    path = getMemberPath(function (s: Foo) {
       return s.bar.baz;
+    });
+    expect(path).toEqual('bar.baz');
+
+    path = getMemberPath(function (s: Foo) {
+      return s['bar']['baz'];
     });
     expect(path).toEqual('bar.baz');
   });
 
-  it('should return properly for mixed', () => {
-    const path = getMemberPath((s: Foo) => {
-      return s.foo;
-    });
-    expect(path).toEqual('foo');
-  });
-
   it('should return properly for properties with return keyword', () => {
-    let path = getMemberPath((s: Foo) => s.returnFoo);
+    let path: ReturnType<typeof getMemberPath>;
+
+    path = getMemberPath((s: Foo) => s.returnFoo);
+    expect(path).toEqual('returnFoo');
+    path = getMemberPath((s: Foo) => s['returnFoo']);
     expect(path).toEqual('returnFoo');
 
     path = getMemberPath((s: Foo) => {
@@ -50,20 +192,40 @@ describe('getMemberPath', () => {
     });
     expect(path).toEqual('returnFoo');
 
+    path = getMemberPath((s: Foo) => {
+      return s['returnFoo'];
+    });
+    expect(path).toEqual('returnFoo');
+
+    path = getMemberPath((s: Foo) => {
+      return s.bar['baz'];
+    });
+    expect(path).toEqual('bar.baz');
+
     path = getMemberPath(function (s: Foo) {
       return s.returnFoo;
     });
     expect(path).toEqual('returnFoo');
-  });
-
-  // TODO: need to implement this
-  it('should return empty string with odd property', () => {
-    let path = getMemberPath((s: Foo) => s['odd-property']);
-    expect(path).toEqual('');
 
     path = getMemberPath(function (s: Foo) {
-      return s['odd-property'];
+      return s['returnFoo'];
     });
-    expect(path).toEqual('');
+    expect(path).toEqual('returnFoo');
+  });
+
+  describe('For non supported use cases', () => {
+    it('should return the wrong output', () => {
+      let path: ReturnType<typeof getMemberPath>;
+
+      path = getMemberPath((s: Foo) => s[' foo ']);
+      expect(path).toEqual('foo');
+
+      path = getMemberPath((s: Foo) => s['odd' + '-' + 'property']);
+      expect(path).toEqual('odd');
+
+      // Since template strings are not allowed
+      path = getMemberPath((s: Foo) => s[`${'odd-property'}`]);
+      expect(path).toEqual('');
+    });
   });
 });

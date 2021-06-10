f7e89d6228215bd77ce2dea2caf166187fa012a3
Chau Tran <nartc7789@gmail.com>
2021-06-10 09:28:21 -0500

    test: fix deep nest test cases

--- packages/integration-test/src/lib/with-classes/deep-nest.spec.ts
+++ packages/integration-test/src/lib/with-classes/deep-nest.spec.ts
@@ -19,36 +19,41 @@ import {
 } from './fixtures/models/deep-nest';
 
 describe('Deep Nest models', () => {
-  it('should map properly', () => {
+  describe('regular', () => {
     const [mapper] = setupClasses('deep-nest');
-    mapper.createMap(FooFooFoo, FooFooFooDto);
-    mapper.createMap(FooFoo, FooFooDto);
-    mapper.createMap(Foo, FooDto);
 
-    const foo = new Foo();
-    foo.foo = new FooFoo();
-    foo.foo.foo = new FooFooFoo();
-    foo.foo.foo.foo = 'some string';
+    it('should map properly', () => {
+      mapper.createMap(FooFooFoo, FooFooFooDto);
+      mapper.createMap(FooFoo, FooFooDto);
+      mapper.createMap(Foo, FooDto);
 
-    const vm = mapper.map(foo, FooDto, Foo);
-    expect(vm).toBeTruthy();
+      const foo = new Foo();
+      foo.foo = new FooFoo();
+      foo.foo.foo = new FooFooFoo();
+      foo.foo.foo.foo = 'some string';
+
+      const vm = mapper.map(foo, FooDto, Foo);
+      expect(vm).toBeTruthy();
+    });
   });
 
-  it('should map with naming conventions', () => {
+  describe('with naming conventions', () => {
     const [mapper] = setupClasses('deep-nest-naming', {
       source: new CamelCaseNamingConvention(),
       destination: new SnakeCaseNamingConvention(),
     });
 
-    mapper.createMap(FooBarBazQux, FooBarBazQuxDto);
-    mapper.createMap(FooBarBaz, FooBarBazDto);
-    mapper.createMap(FooBar, FooBarDto);
+    it('should map with naming conventions', () => {
+      mapper.createMap(FooBarBazQux, FooBarBazQuxDto);
+      mapper.createMap(FooBarBaz, FooBarBazDto);
+      mapper.createMap(FooBar, FooBarDto);
 
-    const vm = mapper.map(
-      { fooBar: { fooBarBaz: { fooBarBazQux: 'some foo bar' } } },
-      FooBarDto,
-      FooBar
-    );
-    expect(vm).toBeTruthy();
+      const vm = mapper.map(
+        { fooBar: { fooBarBaz: { fooBarBazQux: 'some foo bar' } } },
+        FooBarDto,
+        FooBar
+      );
+      expect(vm).toBeTruthy();
+    });
   });
 });

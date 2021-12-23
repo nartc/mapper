ac5030f7811a1c03f5c5c19669d2b3e64fa9cc64
Chau Tran <nartc7789@gmail.com>
2021-12-23 16:28:36 -0600

    fix(core): fallback to primitive if constructor/function is undefined

--- packages/core/src/lib/utils/is-primitive-constructor.util.ts
+++ packages/core/src/lib/utils/is-primitive-constructor.util.ts
@@ -4,6 +4,8 @@
  * @param {Function} value
  */
 export function isPrimitiveConstructor(value: unknown): boolean {
+  // constructor/function passed in is undefined/null, we fall back to primitive
+  if (value == null) return true;
   const proto = Object.getPrototypeOf(value);
   return (
     proto === String ||

798fe6796885f330a6235efb5b62776d1c2499b9
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-25 19:54:54 -0600

    chore(repo): remove mikro test for now

--- packages/integration-test/src/lib/with-mikro/fixtures/profiles/foo.profile.ts
+++
@@ -1,6 +0,0 @@
-import { MappingProfile } from '@automapper/core';
-import { Foo, FooDto } from '../models/foo';
-
-export const simpleFooProfile: MappingProfile = (mapper) => {
-  mapper.createMap(Foo, FooDto);
-};

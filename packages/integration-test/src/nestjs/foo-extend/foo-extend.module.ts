1062fe6975a7feef9e20d7ed8075f2525cbcf69d
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-03-19 02:14:58 -0500

    wip: update packages and ran format

--- packages/integration-test/src/nestjs/foo-extend/foo-extend.module.ts
+++ packages/integration-test/src/nestjs/foo-extend/foo-extend.module.ts
@@ -3,7 +3,7 @@ import { FooExtendController } from './foo-extend.controller';
 import { FooExtendProfile } from './foo-extend.profile';
 
 @Module({
-  controllers: [FooExtendController],
-  providers: [FooExtendProfile],
+    controllers: [FooExtendController],
+    providers: [FooExtendProfile],
 })
 export class FooExtendModule {}

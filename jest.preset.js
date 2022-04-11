c03b59f49b0056e0e05f8e3f63c967ee72d35d70
Chau Tran <nartc7789@gmail.com>
2022-04-11 07:48:31 -0500

    chore(repo): test works with esm

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,14 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+    ...nxPreset,
+    extensionsToTreatAsEsm: ['.ts'],
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+};

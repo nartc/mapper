653cbb7d6aa828a53b7297215d60abce72d2c17e
Chau Tran <nartc7789@gmail.com>
2022-06-10 13:59:38 -0500

    fix(core): cleanup default serializer options

--- packages/core/src/lib/default-serializer-options.ts
+++ packages/core/src/lib/default-serializer-options.ts
@@ -1,18 +1,12 @@
 import { defaultApplyMetadata } from './mappings/apply-metadata';
-import type { Dictionary, MappingStrategyInitializerOptions } from './types';
+import type { MappingStrategyInitializerOptions } from './types';
 
 export const defaultSerializerOptions = {
     applyMetadata: defaultApplyMetadata,
-    preMap<
-        TSource extends Dictionary<TSource>,
-        TDestination extends Dictionary<TDestination>
-    >(source: TSource): TSource {
+    preMap(source) {
         return source;
     },
-    postMap<
-        TSource extends Dictionary<TSource>,
-        TDestination extends Dictionary<TDestination>
-    >(_: TSource, destination: TDestination): TDestination {
+    postMap(_, destination) {
         return destination;
     },
 } as Required<

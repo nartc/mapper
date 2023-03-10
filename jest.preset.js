5cb7741e68cd9a82f1374ab4d8c5270bd8b07dc8
Chau Tran <nartc7789@gmail.com>
2023-03-09 21:16:30 -0600

    chore: upgrade nx

--- jest.preset.js
+++ jest.preset.js
@@ -3,12 +3,18 @@ const nxPreset = require('@nrwl/jest/preset').default;
 module.exports = {
     ...nxPreset,
     extensionsToTreatAsEsm: ['.ts'],
-    globals: {
-        'ts-jest': {
-            useESM: true,
-        },
-    },
+    globals: {},
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
     },
+    /* TODO: Update to latest Jest snapshotFormat
+     * By default Nx has kept the older style of Jest Snapshot formats
+     * to prevent breaking of any existing tests with snapshots.
+     * It's recommend you update to the latest format.
+     * You can do this by removing snapshotFormat property
+     * and running tests with --update-snapshot flag.
+     * Example: "nx affected --targets=test --update-snapshot"
+     * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+     */
+    snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
--- packages/classes/jest.config.cjs
+++ packages/classes/jest.config.cjs
@@ -1,13 +1,14 @@
 module.exports = {
     displayName: 'classes',
     preset: '../../jest.preset.js',
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-        },
-    },
+    globals: {},
     transform: {
-        '^.+\\.[tj]s$': 'ts-jest',
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+            },
+        ],
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageDirectory: '../../coverage/packages/classes',
